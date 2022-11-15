055e6db43756327361e6c42edeacce6f8d0b0b6f
Deve Shanghai <iamasimkhanjadoon@gmail.com>
2022-11-15 15:22:04 +0400

    feat: [Resources] Beyond Ethereum advanced zkSync concepts.

---
+++ crypto-zombies/Advanced Solidity/Oracle Building Part 3/oracle/contracts/HelloBlockchain.sol
@@ -0,0 +1,4 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.4.25 <0.9.0;
+
+contract HelloBlockchain {}
--- crypto-zombies/Advanced Solidity/Oracle Building Part 3/oracle/truffle-config.js
+++ crypto-zombies/Advanced Solidity/Oracle Building Part 3/oracle/truffle-config.js
@@ -1,24 +1,25 @@
-const LoomTruffleProvider = require('loom-truffle-provider')
-
-const path = require('path')
-const fs = require('fs')
-
 module.exports = {
   networks: {
-    extdev: {
-      provider: function () {
-        const privateKey = fs.readFileSync(path.join(__dirname, 'oracle_private_key'), 'utf-8')
-        const chainId = 'extdev-plasma-us1'
-        const writeUrl = 'wss://extdev-plasma-us1.dappchains.com/websocket'
-        const readUrl = 'wss://extdev-plasma-us1.dappchains.com/queryws'
-        return new LoomTruffleProvider(chainId, writeUrl, readUrl, privateKey)
-      },
-      network_id: '474747' //'9545242630824'
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*"
+    },
+    dashboard: {
     }
   },
   compilers: {
     solc: {
-      version: '0.8.4'
+      version: "0.8.13",
+    }
+  },
+  db: {
+    enabled: false,
+    host: "127.0.0.1",
+  },
+  compilers: {
+    solc: {
+      version: '0.5.0'
     }
   }
-}
+};
